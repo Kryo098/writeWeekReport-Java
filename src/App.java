01be9f2094a235a7a7c5c3d6feb831e82f353d52
Kryo098 <ryo0812eng@gmail.com>
2022-07-07 22:50:47 +0900

    週報書け関数　作成

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import java.util.Calendar;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Calendar now = Calendar.getInstance();
+
+        System.out.println(now);
+    }
+}
